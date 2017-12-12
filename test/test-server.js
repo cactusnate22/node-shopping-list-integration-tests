2856db105c2b17318de7db99db104345e4ea2193
Nathan Ash <nash311@gmail.com>
2017-12-12 14:13:19 -0500

    initial commit on tests-for-recipes branch PUT and DELETE tests failing

--- test/test-server.js
+++ test/test-server.js
@@ -138,3 +138,71 @@ describe('Shopping List', function() {
       });
   });
 });
+
+describe ('Recipes', function(){
+  before(function() {
+    return runServer();
+});
+
+  after(function() {
+  return closeServer();
+});
+
+it('should list recipes on GET', function(){
+  return chai.request(app)
+  .get('/recipes')
+  .then(function(res) {
+    res.should.have.status(200);
+    res.should.be.json;
+    res.body.should.be.a('array');
+    res.body.length.should.be.above(0);
+    res.body.forEach(function(item) {
+      item.should.be.a('object');
+      item.should.have.all.keys('id', 'name', 'ingredients');
+    });
+  });
+});
+
+it('should add recipe on POST', function(){
+const newRecipe = {name: 'pancakes', ingredients: ['pancake mix', 'egg', 'water', 'syrup']};
+  return chai.request(app)
+  .post('/recipes')
+  .then(function(res) {
+    res.should.have.status(201);
+    res.should.be.json;
+    res.body.should.be.a('object');
+    res.body.should.include.keys('id', 'name', 'ingredients');
+    res.body.id.should.not.be.null;
+    res.body.should.deep.equal(Object.assign(newRecipe, {id: res.body.id}));
+    });
+  });
+
+  it('should update recipe on PUT', function() {
+    const updateData = {
+      name: 'alphabet soup',
+      ingredients: ['A', 'B', 'C']
+    };
+    return chai.request(app)
+    .get('/recipes')
+    .then(function(res) {
+      updateData.id = res.body[0].id;
+      return chai.request(app)
+      .put(`/recipes/${updateData.id}`)
+      .send(updateData)
+    })
+    .then(function(res) {
+      res.should.have.status(204);
+    });
+});
+  it('should delete recipe on DELETE', function() {
+    return chai.request(app)
+    .get('/recipes')
+    .then(function(res) {
+      return chai.request(app)
+      .delete(`/recipe/${res.body[0].id}`);
+    })
+    then(function(res) {
+      res.should.have.status(204);
+    });
+  });
+});
