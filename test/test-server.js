37f81b1832502c47c8fcb37859e3ad5d640f9998
Nathan Ash <nash311@gmail.com>
2017-12-14 08:07:13 -0500

    POST and DELETE req tests working with chai-http testing

--- test/test-server.js
+++ test/test-server.js
@@ -155,10 +155,11 @@ it('should list recipes on GET', function(){
     res.should.have.status(200);
     res.should.be.json;
     res.body.should.be.a('array');
-    res.body.length.should.be.above(0);
+    res.body.should.have.length.of.at.least(1);
+    // res.body.length.should.be.above(0);
     res.body.forEach(function(item) {
       item.should.be.a('object');
-      item.should.have.all.keys('id', 'name', 'ingredients');
+      item.should.include.keys('id', 'name', 'ingredients');
     });
   });
 });
@@ -167,13 +168,15 @@ it('should add recipe on POST', function(){
 const newRecipe = {name: 'pancakes', ingredients: ['pancake mix', 'egg', 'water', 'syrup']};
   return chai.request(app)
   .post('/recipes')
+  .send(newRecipe)
   .then(function(res) {
     res.should.have.status(201);
     res.should.be.json;
     res.body.should.be.a('object');
     res.body.should.include.keys('id', 'name', 'ingredients');
-    res.body.id.should.not.be.null;
-    res.body.should.deep.equal(Object.assign(newRecipe, {id: res.body.id}));
+    res.body.name.should.equal(newRecipe.name);
+    res.body.ingredients.should.be.a('array');
+    res.body.ingredients.should.include.members(newRecipe.ingredients);
     });
   });
 
@@ -199,7 +202,7 @@ const newRecipe = {name: 'pancakes', ingredients: ['pancake mix', 'egg', 'water'
     .get('/recipes')
     .then(function(res) {
       return chai.request(app)
-      .delete(`/recipe/${res.body[0].id}`);
+      .delete(`/recipes/${res.body[0].id}`);
     })
     then(function(res) {
       res.should.have.status(204);
